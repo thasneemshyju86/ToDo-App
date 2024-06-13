c669caa2b596341fc9a6ac2b781d109b1911fd13
thasneemshyju86 <thasneemshyju86@gmail.com>
2024-06-13 13:19:00 -0700

    add To do items

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useRef,useState } from 'react';
 import todo_icon from '../assets/todo_icon.png';
 import TodoItems from './TodoItems';
 
 const Todo = () => {
+    const inputRef= useRef(null)
+    const [todoList,setTodoList]=useState([])
+
+    // logic to add the text
+    const addText=()=>{
+        const inputText=inputRef.current.value.trim()
+        
+        if(inputText==='') return null;
+        const newTodo={
+            id:Date.now(),
+            text:inputText,
+            isComplete:false
+        }
+        setTodoList((prev)=> [...prev,newTodo])
+        inputRef.current.value=''
+    }
   return (
     <div  className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'> 
        {/*---------title--------  */}
@@ -15,14 +31,16 @@ const Todo = () => {
        {/* ------input box ------ */}
 
        <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-        <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600 ' type='text' placeholder='Add your task'/>
-        <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD</button>
+        <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600 ' type='text' placeholder='Add your task' ref={inputRef}/>
+        <button onClick={addText} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD</button>
        </div>
 
        {/* -------to do list------- */}
 
        <div>
-        <TodoItems/>
+        {todoList.map((item,i)=>{
+            return <TodoItems key={i} text={item.text}/>
+        })}
        </div>
      
     </div>
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -3,14 +3,14 @@ import tick from '../assets/tick.png'
 import not_tick from '../assets/not_tick.png'
 import delete_icon from '../assets/delete.png';
 
-const TodoItems = () => {
+const TodoItems = ({text}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
-        <div>
-            <img src={tick} alt=''/>
-            <p>Learn coding</p>
+        <div className='flex flex-1 items-center cursor-pointer'>
+            <img src={tick} alt='' className='w-7'/>
+            <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
         </div>
-      
+      <img src={delete_icon} alt='' className='w-3.5 cursor-pointer'/>
     </div>
   );
 }
